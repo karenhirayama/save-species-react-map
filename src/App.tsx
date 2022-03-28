bf2ed89cb5e694f0d15bc41f387b2d28657a6b58
Karen Mayumi Hirayama <karenm.hirayama@gmail.com>
2022-03-28 19:31:41 -0300

    Update text align

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css';
+import { About } from './components/about/About';
 import { Header } from './components/header/Header';
 
 function App() {
   return (
     <div className="App"
-    style={{
-      fontFamily: 'Raleway, sans-serif'
-    }}
+      style={{
+        fontFamily: 'Raleway, sans-serif'
+      }}
     >
       <Header />
+      <About />
     </div>
   );
 }
--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import { Box, Container, Typography } from "@mui/material"
+import { Map } from "./map/Map"
 
 export const About = () => {
   return (
-    <div>About</div>
+    <Box>
+      <Container maxWidth='lg'>
+        <Box mt={5}>
+          <Typography variant="h4"
+            sx={{
+              fontFamily: 'Raleway, sans-serif',
+              marginLeft: 1,
+              fontWeight: 600,
+              color: '#005359',
+              textAlign: 'start'
+            }}
+          >
+            Especies Ameaçadas em extinção
+          </Typography>
+          <Box
+            width={400}
+            mt={4}
+          >
+            <Typography
+              sx={{
+                fontFamily: 'Raleway, sans-serif',
+                marginLeft: 1,
+                fontWeight: 700,
+                color: '#005359',
+                textAlign: 'start'
+              }}
+            >
+              A extinção pode ocorrer por diversos motivos, como competição, doenças, catástrofes ambientais, destruição do habitat, mudanças climáticas drásticas, e principalmente pela ação antrópica.
+              {<br />}
+              {<br />}
+              Existem um total de XXXX espécies em ameaça de extinção e esse projeto visa mostrar essas espécies e as suas respectivas áreas.
+              {<br />}
+              {<br />}
+              Também vale ressaltar a importância da conservação das espécies e que esse trabalho de gerenciamento e identificação de espécies ameaçadas é muito importante para que providências sejam tomadas para reduzir as crescentes taxas de extinção.
+            </Typography>
+          </Box>
+        </Box>
+        <Map />
+      </Container>
+    </Box>
   )
 }
