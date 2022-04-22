a4977204a36236fdb5f9200c8baa59f8a2b80319
Karen Mayumi Hirayama <karenm.hirayama@gmail.com>
2022-04-22 15:46:28 -0300

    Add accordion

--- src/components/categories/EvaluationCategories.tsx
+++ src/components/categories/EvaluationCategories.tsx
@@ -1,8 +1,29 @@
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@mui/material"
+import { libEvaluationCategories } from "./libEvaluationCategories"
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 
 export const EvaluationCategories = () => {
+
   return (
-      <>
-      evaluationCategories
-      </>
+    <>
+      {libEvaluationCategories.map((category) => (
+        <Accordion>
+          <AccordionSummary
+            // expendIcon={<ExpandMoreIcon />}
+            id={category.abreviation}
+          >
+            <Typography>
+              {category.label} ({category.abreviation})
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              {category.definition}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </>
   )
 }
