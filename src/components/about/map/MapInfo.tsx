2c018f45f9a758a1dc402371621a5956bb93a57d
Karen Mayumi Hirayama <karenm.hirayama@gmail.com>
2022-04-26 20:59:12 -0300

    Follow guide to show map

--- src/components/about/map/MapInfo.tsx
+++ src/components/about/map/MapInfo.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import { LatLonUfs } from './BrazilStates'
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
 
 export const MapInfo = () => {
+  const geoUrl =
+    "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
   return (
-    <div>MapInfo</div>
+    <>
+      <ComposableMap>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
+          }
+        </Geographies>
+      </ComposableMap>
+    </>
   )
 }
